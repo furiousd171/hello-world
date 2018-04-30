e3e72e0566e683a83db9fe20c013d656d42882ea
furiousd171 <jon.grevstad@pcc.edu>
2018-04-30 14:13:25 -0700

    Added author

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,6 +2,11 @@ package edu.pcc.cis.helloworld;
 
 import java.util.Date;
 
+/**
+ * @author      Jon Grevstad
+ * @version     4/30/2018
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
